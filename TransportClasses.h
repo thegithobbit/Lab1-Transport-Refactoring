05ea5172e91642ff96c70b21e74acb97a3c3edd5
thegithobbit <chevikleyla@gmail.com>
2026-02-26 19:15:58 +0200

    Update TransportClasses.h

--- TransportClasses.h
+++ TransportClasses.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -24,6 +25,8 @@ public:
 
     double getSpeed() const { return speed; }
     string getName() const { return name; }
+    string getType() const { return type; }
+    
     virtual int getPriority() const { return static_cast<int>(speed); }
 
     virtual void printInfo() const {
@@ -31,32 +34,102 @@ public:
              << ", Fuel: " << fuel << ", Speed: " << speed << ", Weight: " << weight << endl;
     }
 
-    // застосуємо поліморфні методи замість dynamic_cast
     virtual void performSpecialAction1() { honk(); }
-    virtual void performSpecialAction2() { cout << name << " performs default action.\n"; }
+    virtual void performSpecialAction2() { cout << name << " is maintaining its route.\n"; }
 
     virtual void honk() const { cout << name << " honks!\n"; }
 };
 
-class Car : public Transport {
-    double enginePower;
+class LandTransport : public Transport {
+protected:
+    int wheels;
+    int passengers;
+
 public:
-    Car(string n, int num, string f, double s, double w, int p, double ep)
-        : Transport("Car", n, num, f, s, w), enginePower(ep) {}
+    LandTransport(string t, string n, int num, string f, double s, double w, int wh, int p)
+        : Transport(t, n, num, f, s, w), wheels(wh), passengers(p) {}
 
     void printInfo() const override {
         Transport::printInfo();
-        cout << "Engine Power: " << enginePower << " HP\n";
+        cout << "Wheels: " << wheels << ", Passengers: " << passengers << endl;
     }
+};
 
-    void performSpecialAction1() override { turboBoost(); }
-    void performSpecialAction2() override { drift(); }
+class Car : public LandTransport {
+    double enginePower;
+public:
+    Car(string n, int num, string f, double s, double w, int p, double ep)
+        : LandTransport("Car", n, num, f, s, w, 4, p), enginePower(ep) {}
 
-    void turboBoost() { speed *= 1.2; cout << name << " activated turbo! Speed: " << speed << endl; }
-    void drift() { cout << name << " is drifting!\n"; }
-    
+    void performSpecialAction1() override { 
+        speed *= 1.2; 
+        cout << name << " activated Turbo! New speed: " << speed << endl; 
+    }
+    void performSpecialAction2() override { cout << name << " is drifting!\n"; }
     int getPriority() const override { return static_cast<int>(speed + enginePower/10); }
 };
 
-#endif
+class Bus : public LandTransport {
+    double routeLength;
+public:
+    Bus(string n, int num, string f, double s, double w, int p, double rl)
+        : LandTransport("Bus", n, num, f, s, w, 6, p), routeLength(rl) {}
 
+    void performSpecialAction1() override { cout << name << " doors opened for passengers.\n"; }
+    void performSpecialAction2() override { cout << name << " announces: Next stop is University.\n"; }
+    int getPriority() const override { return passengers; }
+};
+
+class Plane : public Transport {
+    double wingspan;
+    double maxAltitude;
+public:
+    Plane(string n, int num, string f, double s, double w, double ws, double ma)
+        : Transport("Plane", n, num, f, s, w), wingspan(ws), maxAltitude(ma) {}
+
+    void performSpecialAction1() override { cout << name << " is taking off into the clouds!\n"; }
+    void performSpecialAction2() override { cout << name << " reached altitude: " << maxAltitude << "m\n"; }
+    int getPriority() const override { return static_cast<int>(speed + maxAltitude/1000); }
+};
+
+class Boat : public Transport {
+    double length;
+    double draft;
+public:
+    Boat(string n, int num, string f, double s, double w, double l, double d)
+        : Transport("Boat", n, num, f, s, w), length(l), draft(d) {}
+
+    void performSpecialAction1() override { cout << name << " is docking at the pier.\n"; }
+    void performSpecialAction2() override { cout << name << " is sailing across the lake.\n"; }
+};
+
+class Truck : public LandTransport {
+    double cargoCapacity;
+public:
+    Truck(string n, int num, string f, double s, double w, int p, double cc)
+        : LandTransport("Truck", n, num, f, s, w, 6, p), cargoCapacity(cc) {}
+
+    void performSpecialAction1() override { cout << name << " is loading " << cargoCapacity << " tons of cargo.\n"; }
+    int getPriority() const override { return static_cast<int>(cargoCapacity + speed/2); }
+};
+
+class Bicycle : public LandTransport {
+public:
+    Bicycle(string n, int num, double s, double w)
+        : LandTransport("Bicycle", n, num, "None", s, w, 2, 1) {}
+
+    void performSpecialAction1() override { cout << name << " is pedaling hard!\n"; }
+    void performSpecialAction2() override { cout << name << " rings the bell: Ring-ring!\n"; }
+};
+
+class Train : public LandTransport {
+    int carriages;
+public:
+    Train(string n, int num, string f, double s, double w, int p, int c)
+        : LandTransport("Train", n, num, f, s, w, 12, p), carriages(c) {}
+
+    void performSpecialAction1() override { cout << name << " is departing from the station.\n"; }
+    int getPriority() const override { return passengers + carriages; }
+};
+
+#endif
